54e1c212049fa6a715a459dfb4233be617bb90d1
AWAIS SADDIQUI <awaissaddiqui143@gmail.com>
2022-08-22 03:18:11 -0700

    The weather app is completed....

--- script.js
+++ script.js
@@ -35,10 +35,14 @@ const getWheather = () => {
             // wind speed
             const wind = json.current.wind_kph;
             document.getElementById("wind").innerHTML = "Wind Speed = " + wind + " km/h";
-            // crruent date and time
+            // current date and time
             const date = json.location.localtime;
-            document.getElementById("date").innerHTML = "Date = " + date ;              
-            // button dissabled
+            // const newDate = date.
+            document.getElementById("date").innerHTML = "Date = " + date  ;      
+            //result section
+            const result = document.querySelector("#result");
+            result.setAttribute("style", "width: 400px; height: 300px; margin-left: 370px;  background-color: white; border: 2px solid black; border-radius: 25px;")        
+           // button disabled
             document.querySelector(".btn").disabled = true;
         }) 
 
